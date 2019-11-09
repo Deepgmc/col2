20fc6fcfa9f1d455862fbe3ba8b7d10e3888c44a
Deepgmc <deepgmc_job@mail.ru>
2019-11-09 16:06:44 +0300

    node_modules update

---
+++ webpack.config.js
@@ -0,0 +1,27 @@
+let path = require('path');
+
+const login = {
+    watch: true,
+    mode: 'development',
+    devtool: 'source-map',
+    entry: path.resolve(__dirname, 'src') + '/login.js',
+
+    output: {
+        path: path.resolve(__dirname, 'public'),
+        filename: 'login.js',
+        publicPath: '/'
+    },
+    module: {
+        rules: [
+            { test: /\.css$/, exclude: '/node_modules', use: ['style-loader', 'css-loader'] },
+            { test: /\.js$/, exclude: '/node_modules', use: 'babel-loader' },
+            {
+                test: /\.(png|svg|jpg|gif)$/,
+                exclude: '/node_modules',
+                use: ['file-loader']
+            }
+        ]
+    }
+}
+
+module.exports = login
